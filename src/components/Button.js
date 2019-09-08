203af4b6ec14f18e7df70a6ee02521339353e75b
julio <sandronister@gmail.com>
2019-09-07 21:48:52 -0300

    button event

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,19 +3,37 @@ import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native'
 
 const styles = StyleSheet.create({
 	button: {
-		fontSize:30,
-		width: Dimensions.get('window').width/4,
-		height:  Dimensions.get('window').height/7,
+		fontSize: 30,
+		width: Dimensions.get('window').width / 4,
+		height: Dimensions.get('window').height / 7,
 		padding: 20,
 		backgroundColor: '#f0f0f0',
 		textAlign: 'center',
 		borderWidth: 1,
 		borderColor: '#888'
+	},
+	operationButton: {
+		color: '#FFF',
+		backgroundColor: '#FA8231'
+	},
+	buttonDouble: {
+		width: (Dimensions.get('window').width / 4) * 2
+	},
+	buttonTriple: {
+		width: (Dimensions.get('window').width / 4) * 3
 	}
-});
+})
 
-export default (props) => (
-	<TouchableHighlight onPress={props.onClick}>
-		<Text style={styles.button}>{props.label}</Text>
-	</TouchableHighlight>
-)
+export default (props) => {
+    const styleButton = [styles.button]
+
+    if(props.double) styleButton.push(styles.buttonDouble)
+    if(props.triple) styleButton.push(styles.buttonTriple)
+    if(props.operation) styleButton.push(styles.operationButton)
+
+	return (
+		<TouchableHighlight onPress={ ()=> props.onClick(props.label) }>
+			<Text style={styleButton}>{props.label}</Text>
+		</TouchableHighlight>
+	)
+}
